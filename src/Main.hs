2221dba7bbe2d319607f4f36c0cca172692dd397
lopezfjose <jflopezfernandez@gmail.com>
2018-11-21 19:57:46 -0500

    Defining data types and structure

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,28 @@
 
---
+data Article = Article String
+    deriving (Show, Eq)
+
+data Noun = Noun String
+    deriving (Show, Eq)
+
+data Verb = Verb String
+    deriving (Show, Eq)
+
+data Adjective = Adjective String
+    deriving (Show, Eq)
+
+data Adverb = Adverb String
+    deriving (Show, Eq)
+
+data NounPhrase = NounPhrase Noun Verb
+    deriving (Show, Eq)
+
+data VerbPhrase = VerbPhrase Verb NounPhrase
+    deriving (Show, Eq)
+
+data Sentence = Sentence NounPhrase VerbPhrase
 
 
 main :: IO ()
 main = do
-    putStrLn "<Testing...>"
+    putStrLn "None"
